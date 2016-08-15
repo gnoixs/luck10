a9a18dd39927026befbe2403619998b9aa15bcae
feaco <pheaco@gmail.com>
2016-08-15 18:43:04 +0800

    ok

--- config/config.gulp.js
+++ config/config.gulp.js
@@ -5,18 +5,18 @@ var Config = function() {
 	this.gulp = {
 		"clean"  :   [this.bulid,this.deploy],   
 		"style"  : {
-			"src"   : this.src    + "/sass/",
-			"build" : this.build  + "/style/",
+			"src"   : this.src     + "/sass/",
+			"build" : this.build   + "/style/",
 			"deploy" : this.deploy + "/style/",
 		},
 		"scripts" : {
-			"src"   : this.src    + "/scripts/",
-			"build" : this.build  + "/scripts/",
+			"src"   : this.src     + "/scripts/",
+			"build" : this.build   + "/scripts/",
 			"deploy" : this.deploy + "/scripts/"
 		},
 		"images" : {
-			"src"   : this.src    + "/imgs/**/*.{jpg,png,gif,jpeg}",
-			"build" : this.build  + "/imgs/",
+			"src"   : this.src     + "/imgs/**/*.{jpg,png,gif,jpeg}",
+			"build" : this.build   + "/imgs/",
 			"deploy" : this.deploy + "/imgs/"
 		}
 	}
--- public/src/scripts/commons/boxset.js
+++ public/src/scripts/commons/boxset.js
@@ -1,10 +1,19 @@
 $(function(){
-	$('.outer').css({
-		'width':window.innerWidth+'px',
-		'height':window.innerHeight+'px',
-	});
-	$('.outer .inner').css({
-		'zoom':window.innerWidth/640,
-		'visibility':'visible'
-	});
+	$(window).on('ready resize',function(){
+		//外层容器
+		$('.outer').css({
+			'width':window.innerWidth+'px',
+			'height':window.innerHeight+'px',
+		});
+		//内层页面
+		$('.outer .page').css({
+			'zoom':window.innerWidth/640,
+			'visibility':'visible'
+		});
+		//选球区容器
+		$('.sections').css({
+			'height':(window.innerHeight*640/window.innerWidth-($('.nav').height()+$('.nav-down').height()+$('.footer').height()))+'px',
+			'top':($('.nav').height()+$('.nav-down').height())+'px'
+		});
+	})
 })
